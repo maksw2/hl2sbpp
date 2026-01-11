27259763667e5161a300fae9c5db387677c99bc6
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-01-11 14:00:45 +0200

    Update linux compile files

--- compile-dbg.sh
+++ compile-dbg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./waf configure -T debug --use-ccache --togles --64bits --prefix=../game --build-games=hl2sbpp --disable-warns
-./waf build install -p -vv
+./waf build install -p -vv -j$(nproc)
